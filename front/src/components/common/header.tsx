bd4fc012eb644d1c96f7adefe0836b0afd302722
hyeonseong <daeho3@naver.com>
2025-04-22 17:43:40 +0900

    💄(1)로고 이미지 추가 (2)header에 로고 적용

--- front/src/components/common/header.tsx
+++ front/src/components/common/header.tsx
@@ -5,23 +5,14 @@ import { Button } from '@/components/ui/button';
 
 export function Header() {
   return (
-    <header className="w-full border-b">
+    <header className="w-full border-b !border-black/[0.08] bg-white">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         {/* 로고 */}
         <Link href="/" className="text-xl font-bold">
-          florary
+          <img src="/logo.svg" alt="logo" className="w-40" />
         </Link>
-
-        {/* 로그인/회원가입 버튼 */}
-        <div className="flex gap-2">
-          <Button variant="ghost" size="sm">
-            로그인
-          </Button>
-          <Button variant="outline" size="sm">
-            회원가입
-          </Button>
-        </div>
       </div>
     </header>
   );
-} 
+}
+
